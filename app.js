25eab62e136083f0e0ee26d4de3306dd345604bd
Janusz Wendland <januszwendland@gmail.com>
2020-05-14 14:43:09 +0200

    Show days from last login

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-/*global Vue, axios*/
+/*global Vue, axios, moment*/
 
 Vue.component('member', {
    template:
     `<div class="row border-bottom pb-1 mb-1">
-        <div v-html="rank" class="col-4"></div>
-        <div v-html="member.name" class="col-4"></div>
-        <div v-html="last" class="col-4"></div>
+        <div v-html="rank" class="col-3"></div>
+        <div v-html="member.name" class="col-3"></div>
+        <div v-html="last" class="col-3"></div>
+        <div v-html="days" class="col-3"></div>
     </div>`,
     
     props: ['member', 'rank'],
@@ -13,17 +14,39 @@ Vue.component('member', {
     data: function () {
         return {
             last: null,
-            name: null
+            name: null,
+            days: null,
+            today: null
         }
     },
     
     mounted: function () {
         this.name = this.member.name;
         
+        function formatDate(date) {
+            let d = new Date(date),
+                month = '' + (d.getMonth() + 1),
+                day = '' + d.getDate(),
+                year = d.getFullYear();
+
+            if (month.length < 2) {
+                month = '0' + month;
+            }
+            
+            if (day.length < 2) {
+                day = '0' + day;
+            }
+
+            return [year, month, day].join('-');
+        }
+        
+        this.today = formatDate(new Date());
+        
         axios
             .get('https://api.tibiadata.com/v2/characters/' + this.name.replace(' ', '+') + '.json')
             .then(response => {
-                this.last = response.data.characters.data.last_login[0].date.replace('.000000', '');
+                this.last = response.data.characters.data.last_login[0].date.substring(0, 10);
+                this.days = (new Date(this.today) - new Date(this.last)) / (1000 * 60 * 60 * 24);
             })
     }
 });
@@ -36,6 +59,8 @@ var app = new Vue({
     },
     methods: {
         check: function () {
+            this.guild = null;
+            
             if (this.guildName.length > 2) {
                 axios
                     .get('https://api.tibiadata.com/v2/guild/' + this.guildName.replace(' ', '+') + '.json')
