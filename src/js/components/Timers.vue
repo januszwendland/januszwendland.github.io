30c4abe12057e74edaf9adec1a106acad944b128
Janusz Wendland <januszwendland@gmail.com>
2021-09-22 18:28:07 +0200

    Timers - cooldown fix

--- src/js/components/Timers.vue
+++ src/js/components/Timers.vue
@@ -170,14 +170,14 @@
                         { name: 'Black Vixen', img: 'img/timers/black-vixen.gif', cooldown: 20, on: false, time: '00:00:00', interval: null },
                         { name: 'Bloodback', img: 'img/timers/bloodback.gif', cooldown: 20, on: false, time: '00:00:00', interval: null },
                         { name: 'Darkfang', img: 'img/timers/darkfang.gif', cooldown: 20, on: false, time: '00:00:00', interval: null },
-                        { name: 'Shadowpelt', img: 'img/timers/shadowpelt.gif', cooldown: 4, on: false, time: '00:00:00', interval: null },
-                        { name: 'Sharpclaw', img: 'img/timers/sharpclaw.gif', cooldown: 4, on: false, time: '00:00:00', interval: null }
+                        { name: 'Shadowpelt', img: 'img/timers/shadowpelt.gif', cooldown: 20, on: false, time: '00:00:00', interval: null },
+                        { name: 'Sharpclaw', img: 'img/timers/sharpclaw.gif', cooldown: 20, on: false, time: '00:00:00', interval: null }
                     ],
                     'Werelions bosses': [
                         { name: 'Katex Blood Tongue', img: 'img/timers/katex-blood-tongue.gif', cooldown: 20, on: false, time: '00:00:00', interval: null },
                         { name: 'Srezz Yellow Eyes', img: 'img/timers/srezz-yellow-eyes.gif', cooldown: 20, on: false, time: '00:00:00', interval: null },
                         { name: 'Utua Stone Sting', img: 'img/timers/utua-stone-sting.gif', cooldown: 20, on: false, time: '00:00:00', interval: null },
-                        { name: 'Yirkas Blue Scales', img: 'img/timers/yirkas-blue-scales.gif', cooldown: 4, on: false, time: '00:00:00', interval: null }
+                        { name: 'Yirkas Blue Scales', img: 'img/timers/yirkas-blue-scales.gif', cooldown: 20, on: false, time: '00:00:00', interval: null }
                     ]
                 },
                 chars: [],
