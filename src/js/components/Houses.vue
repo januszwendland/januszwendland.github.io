ba2d94a81004adc5abbb6d027af54a2ffda13698
Janusz Wendland <januszwendland@gmail.com>
2022-03-28 22:34:32 +0200

    New worlds

--- src/js/components/Houses.vue
+++ src/js/components/Houses.vue
@@ -31,6 +31,15 @@
                 </div>
                 <small class="filters-info">Filter by town.</small>
             </div>
+            <div class="mt"
+                v-if="selectedServer">
+                <div class="filters-pills">
+                    <span class="filters-pill"
+                        v-bind:class="{ 'selected': isBidded }"
+                        v-on:click="showBidded()">Is bidded</span>
+                </div>
+                <small class="filters-info">Show only houses with bid</small>
+            </div>
             <template
                 v-for="(town, name) in server"
                 v-bind:key="name">
@@ -50,7 +59,6 @@
                         <template
                             v-for="(house, index) in town"
                             v-bind:key="index">
-                            {{ key }}
                             <tr v-if="house.status !== 'rented'">
                                 <td>
                                     <a v-bind:href="'https://www.tibia.com/community/?subtopic=houses&page=view&world=' + selectedServer + '&houseid=' + house.house_id" target="_blank">{{ house.name }}</a>
@@ -80,9 +88,106 @@
 
         data: function () {
             return {
-                servers: [ 'Adra', 'Alumbra', 'Antica', 'Astera', 'Belobra', 'Bona', 'Calmera', 'Carnera', 'Celebra', 'Celesta', 'Concorda', 'Damora', 'Descubra', 'Dibra', 'Emera', 'Epoca', 'Estela', 'Famosa', 'Fera', 'Ferobra', 'Fervora', 'Firmera', 'Garnera', 'Gentebra', 'Gladera', 'Harmonia', 'Honbra', 'Impera', 'Inabra', 'Javibra', 'Juva', 'Kalibra', 'Karna', 'Kenora', 'Libertabra', 'Lobera', 'Luminera', 'Lutabra', 'Marcia', 'Menera', 'Mercera', 'Mitigera', 'Monza', 'Mudabra', 'Nefera', 'Nexa', 'Nossobra', 'Ombra', 'Optera', 'Pacembra', 'Pacera', 'Peloria', 'Premia', 'Quelibra', 'Quintera', 'Ragna', 'Refugia', 'Reinobra', 'Relania', 'Relembra', 'Secura', 'Serdebra', 'Serenebra', 'Solidera', 'Talera', 'Tembra', 'Unica', 'Unisera', 'Utobra', 'Venebra', 'Visabra', 'Vunira', 'Wintera', 'Wizera', 'Xandebra', 'Xylona', 'Yonabra', 'Ysolera', 'Zenobra', 'Zuna', 'Zunera' ],
+                servers: [
+                    'Adra',
+                    'Alumbra',
+                    'Antica',
+                    'Ardera',
+                    'Astera',
+                    'Bastia',
+                    'Batabra',
+                    'Belobra',
+                    'Bona',
+                    'Cadebra',
+                    'Calmera',
+                    'Celebra',
+                    'Celesta',
+                    'Collabra',
+                    'Damora',
+                    'Descubra',
+                    'Dibra',
+                    'Epoca',
+                    'Esmera',
+                    'Famosa',
+                    'Fera',
+                    'Ferobra',
+                    'Firmera',
+                    'Gentebra',
+                    'Gladera',
+                    'Harmonia',
+                    'Havera',
+                    'Honbra',
+                    'Illusera',
+                    'Impulsa',
+                    'Inabra',
+                    'Issobra',
+                    'Kalibra',
+                    'Karna',
+                    'Libertabra',
+                    'Lobera',
+                    'Luminera',
+                    'Lutabra',
+                    'Marbera',
+                    'Marcia',
+                    'Menera',
+                    'Monza',
+                    'Mudabra',
+                    'Mykera',
+                    'Nadora',
+                    'Nefera',
+                    'Nossobra',
+                    'Ocebra',
+                    'Olima',
+                    'Ombra',
+                    'Optera',
+                    'Pacera',
+                    'Peloria',
+                    'Premia',
+                    'Quelibra',
+                    'Quintera',
+                    'Refugia',
+                    'Reinobra',
+                    'Seanera',
+                    'Secura',
+                    'Serdebra',
+                    'Solidera',
+                    'Suna',
+                    'Talera',
+                    'Tembra',
+                    'Thyria',
+                    'Trona',
+                    'Utobra',
+                    'Venebra',
+                    'Versa',
+                    'Visabra',
+                    'Vunira',
+                    'Wintera',
+                    'Wizera',
+                    'Xandebra',
+                    'Yonabra',
+                    'Zenobra',
+                    'Zuna',
+                    'Zunera'
+                ],
                 selectedServer: '',
-                towns: [ 'Ab\'Dendriel', 'Ankrahmun', 'Carlin', 'Darashia', 'Edron', 'Farmine', 'Gray Beach', 'Issavi', 'Kazordoon', 'Liberty Bay', 'Port Hope', 'Rathleton', 'Svargrond', 'Thais', 'Venore', 'Yalahar' ],
+                towns: [
+                    'Ab\'Dendriel',
+                    'Ankrahmun',
+                    'Carlin',
+                    'Darashia',
+                    'Edron',
+                    'Farmine',
+                    'Gray Beach',
+                    'Issavi',
+                    'Kazordoon',
+                    'Liberty Bay',
+                    'Port Hope',
+                    'Rathleton',
+                    'Svargrond',
+                    'Thais',
+                    'Venore',
+                    'Yalahar'
+                ],
                 server: {
                     'Ab\'Dendriel': null,
                     'Ankrahmun': null,
@@ -101,14 +206,53 @@
                     'Venore': null,
                     'Yalahar' : null
                 },
+                serverFree: {
+                    'Ab\'Dendriel': null,
+                    'Ankrahmun': null,
+                    'Carlin': null,
+                    'Darashia': null,
+                    'Edron': null,
+                    'Farmine': null,
+                    'Gray Beach': null,
+                    'Issavi': null,
+                    'Kazordoon': null,
+                    'Liberty Bay': null,
+                    'Port Hope': null,
+                    'Rathleton': null,
+                    'Svargrond': null,
+                    'Thais': null,
+                    'Venore': null,
+                    'Yalahar' : null
+                },
+                serverBidded: {
+                    'Ab\'Dendriel': null,
+                    'Ankrahmun': null,
+                    'Carlin': null,
+                    'Darashia': null,
+                    'Edron': null,
+                    'Farmine': null,
+                    'Gray Beach': null,
+                    'Issavi': null,
+                    'Kazordoon': null,
+                    'Liberty Bay': null,
+                    'Port Hope': null,
+                    'Rathleton': null,
+                    'Svargrond': null,
+                    'Thais': null,
+                    'Venore': null,
+                    'Yalahar' : null
+                },
                 filterByTown: null,
+                isBidded: false,
                 loader: 0
             }
         },
 
         methods: {
             checkServer: function() {
-                let server = this.server;
+                let serverFree = this.serverFree,
+                    serverBidded = this.serverBidded;
+
                 this.filterByTown = null;
 
                 if (this.selectedServer) {
@@ -117,12 +261,15 @@
                         axios
                             .get('https://api.tibiadata.com/v3/houses/' + this.selectedServer + '/' + town)
                             .then(response => {
-                                server[town] = response.data.houses.house_list.filter(a => a.rented !== true);
+                                serverFree[town] = response.data.houses.house_list.filter(a => a.rented !== true);
+                                serverBidded[town] = response.data.houses.house_list.filter(a => a.rented !== true).filter(a => a.auction.time_left !== '');
 
                                 this.loader--;
                             })
                     });
                 }
+
+                this.server = serverFree;
             },
             filter: function(town) {
                 if (town === this.filterByTown) {
@@ -131,9 +278,19 @@
                     this.filterByTown = town;
                 }
             },
+            showBidded: function() {
+                this.isBidded = !this.isBidded;
+
+                if (this.isBidded) {
+                    this.server = this.serverBidded;
+                } else {
+                    this.server = this.serverFree;
+                }
+            },
             formatNumber: function (number) {
                 return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             }
         }
     }
 </script>
+
